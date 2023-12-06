378a696d58c11a18f9237e15c5cca7fddeb60d89
LouisLalonde <louis.lalonde@umontreal.ca>
2023-12-06 10:02:34 -0500

    Changed the functions with double undescores to single underscore

--- migration/tests/test_relis_statistics_kernel.py
+++ migration/tests/test_relis_statistics_kernel.py
@@ -1,4 +1,5 @@
 import re
+import os
 import numpy as np
 import pandas as pd
 import pytest
@@ -10,8 +11,15 @@ from matplotlib import ticker
 from matplotlib.text import Text
 from statsmodels.robust.scale import mad
 from scipy.stats import kurtosis, skew, shapiro, spearmanr, pearsonr, chi2_contingency
+from python.relis_statistics_kernel import (
+    NominalVariables, ContinuousVariables, _aggregate_variables_by_data_type
+)
 
 ### Testing
+
+CURRENT_FILE_PATH = os.path.abspath(__file__)
+
+TEST_ROOT_DIRECTORY = os.path.dirname(CURRENT_FILE_PATH)
 
 AGGREGATED_NOMINAL_VARIABLES = {'Venue': 'venue',
                                 'Search Type': 'search_type',
@@ -26,59 +34,17 @@ AGGREGATED_NOMINAL_VARIABLES = {'Venue': 'venue',
 AGGREGATED_CONTINUOUS_VARIABLES = {'Publication year': 'publication_year',
                                     'Targeted year': 'targeted_year'}
 
-### Config
-
-plt.rcParams['figure.max_open_warning'] = 0
-
-custom = {'axes.edgecolor': 'black', 'grid.linestyle': 'dashed', 'grid.color': 'grey'}
-
-sns.set_style('darkgrid', rc = custom)
-
-class Multivalue(Enum):
-    SEPARATOR = '|'
-
-class Policies(Enum):
-    DROP_NA = False
-
-### Types
-
-class VariableClassificationType(Enum):
-    NOMINAL = 'Nominal'
-    CONTINUOUS = 'Continuous'
-
-class Variable:
-    def __init__(self, name: str, title: str, data_type: VariableClassificationType, multiple: bool):
-        self.name = name
-        self.title = title
-        self.data_type = data_type
-        self.multiple = multiple
-
-class NominalVariables(Enum):
-    venue = Variable('venue', 'Venue', VariableClassificationType.NOMINAL, False)
-    search_type = Variable('search_type', 'Search Type', VariableClassificationType.NOMINAL, False)
-    domain = Variable('domain', 'Domain', VariableClassificationType.NOMINAL, False)
-    transformation_language = Variable('transformation_language', 'Transformation Language', VariableClassificationType.NOMINAL, True)
-    source_language = Variable('source_language', 'Source language', VariableClassificationType.NOMINAL, False)
-    target_language = Variable('target_language', 'Target language', VariableClassificationType.NOMINAL, False)
-    scope = Variable('scope', 'Scope', VariableClassificationType.NOMINAL, True)
-    industrial = Variable('industrial', 'Industrial', VariableClassificationType.NOMINAL, False)
-    bidirectional = Variable('bidirectional', 'Bidirectional', VariableClassificationType.NOMINAL, False)
-
-class ContinuousVariables(Enum):
-    publication_year = Variable('publication_year', 'Publication year', VariableClassificationType.CONTINUOUS, False)
-    targeted_year = Variable('targeted_year', 'Targeted year', VariableClassificationType.CONTINUOUS, False)
-
 @pytest.fixture
 def project_classification_data():
-    return pd.read_csv('./data/relis_classification_CV.csv', encoding='utf8')
+    return pd.read_csv(f'{TEST_ROOT_DIRECTORY}/data/relis_classification_CV.csv', encoding='utf8')
 
 @pytest.fixture
 def project_classification_nominal_data():
-    return pd.read_csv('./data/project_classification_nominal_data.csv', encoding='utf8')
+    return pd.read_csv(f'{TEST_ROOT_DIRECTORY}/data/project_classification_nominal_data.csv', encoding='utf8')
 
 @pytest.fixture
 def project_classification_continuous_data():
-    return pd.read_csv('./data/project_classification_continuous_data.csv', encoding='utf8')
+    return pd.read_csv(f'{TEST_ROOT_DIRECTORY}/data/project_classification_continuous_data.csv', encoding='utf8')
 
 @pytest.fixture
 def nominal_variables():
@@ -100,9 +66,6 @@ def aggregated_continuous_variables():
 
 ## Parsing
 
-def aggregate_variables_by_data_type(variables: type[NominalVariables] | type[ContinuousVariables]):
-    return {variable.value.title: variable.name for variable in variables} # type: ignore
-
 def test_load_classification_data(project_classification_data: pd.DataFrame):
     # Assert that classification file isn't empty
     assert(project_classification_data.size) > 0
@@ -114,10 +77,10 @@ def test_load_classification_data(project_classification_data: pd.DataFrame):
     assert(hasattr(project_classification_data, 'Publication year'))
 
 def test_aggregate_nominal_variables(nominal_variables, aggregated_nominal_variables):
-    assert aggregate_variables_by_data_type(nominal_variables) == aggregated_nominal_variables
+    assert _aggregate_variables_by_data_type(nominal_variables) == aggregated_nominal_variables
 
 def test_aggregate_continuous_variables(continuous_variables, aggregated_continuous_variables):
-    assert aggregate_variables_by_data_type(continuous_variables) == aggregated_continuous_variables
+    assert _aggregate_variables_by_data_type(continuous_variables) == aggregated_continuous_variables
 
 ## Preprocessing
 
@@ -136,7 +99,7 @@ def test_processing_classification_continuous_data(project_classification_data, 
     assert project_classification_continuous_data.equals(data)
     
 
-def substitute_nan(df: pd.DataFrame) -> None:
+def substitute_nan(df: pd.DataFrame):
     df.replace(np.nan, '', inplace=True)
 
 def create_test_substitute_nan(df: pd.DataFrame):
